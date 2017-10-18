f373d424d01424aa0a0da1e742d0bdf62d6efd2d
Eduardo Meirelles <emeirell@hotmail.com>
2017-10-18 11:13:58 -0200

    full application

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM php:7.0-apache
+COPY cna.jpg /var/www/html/
+COPY index.php /var/www/html/
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Welcome!</title>
+  <style>
+       body {
+           width: 35em;
+            margin: 0 auto;
+              font-family: Tahoma, Verdana, Arial, sans-serif;
+      }
+
+       img {
+         margin-left: auto;
+       margin-right: auto;
+      }
+  </style>
+ </head>
+ <body>
+  <h1>Welcome to demo Webpage!</h1>
+  <p> This service has been provide by container: <b> <?php echo gethostname(); ?> </b></p>
+  <p>If you see this page, the web server is successfully installed and working. Further configuration might be required.</p>
+
+  <p>For more vSphere Integrated Containers info such as Wiki's, guides and code itself - head to
+  <a href="https://github.com/vmware/vic">GitHub</a>.<br/>
+  <img src="cna.jpg" alt="Cloud Native">
+
+  <p>For online documentation and support for VIC  please refer to
+  <a href="https://www.vmware.com/products/vsphere/integrated-containers.html">vmware.com/vic</a>.<br/>
+
+ <p> Dont miss any news, follow us on
+ <a href="https://twitter.com/cloudnativeapps/">Twitter</a>
+ and
+ <a href="https://blogs.vmware.com/cloudnative/">CNA Blog</a>.<br/> </p>
+
+  <p><em>Thank you for using vSphere Integrated Containers.</em></p>
+ </body>
+</html>
